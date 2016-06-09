2318bbe24db7b848b2122062cdbf284c90035706
Andreas <dr.reser@googlemail.com>
2016-06-09 22:34:55 +0200

    Test 0013

--- Rmd/knitPosts.R
+++ Rmd/knitPosts.R
@@ -5,8 +5,9 @@
 
 
 library(knitr)
-setwd('/Users/Andreas/git/dr-reser.github.io')
+setwd('/Users/Andreas/git/dr-reser.github.io/_posts')
 opts_knit$set(root.dir = '/Users/Andreas/git/dr-reser.github.io')
-opts_chunk$set(comment=NA, fig.width=6, fig.height=6, fig.path = 'images/First-Post/')
+opts_chunk$set(comment=NA, fig.width=6, fig.height=6, fig.path = 'figures/')
 knit('./Rmd/2016-06-08-First-Post.Rmd', './_posts/2016-06-08-First-Post.md')
 
+knit('2016-06-08-First-Post.Rmd')
