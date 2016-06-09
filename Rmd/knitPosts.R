39ea512981f8e01bcfe135d6386936f285270f93
Andreas <dr.reser@googlemail.com>
2016-06-09 22:26:41 +0200

    Test 0012

--- Rmd/knitPosts.R
+++ Rmd/knitPosts.R
@@ -10,5 +10,3 @@ opts_knit$set(root.dir = '/Users/Andreas/git/dr-reser.github.io')
 opts_chunk$set(comment=NA, fig.width=6, fig.height=6, fig.path = 'images/First-Post/')
 knit('./Rmd/2016-06-08-First-Post.Rmd', './_posts/2016-06-08-First-Post.md')
 
-opts_knit$get('root.dir')
-
